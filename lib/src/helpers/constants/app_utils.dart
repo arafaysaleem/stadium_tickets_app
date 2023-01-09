90f487799bebe072520f65ff70ae31a0c49863f6
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2023-01-09 22:44:00 +0500

    chnage card regex

--- lib/src/helpers/constants/app_utils.dart
+++ lib/src/helpers/constants/app_utils.dart
@@ -137,7 +137,7 @@ class Regexes {
 
   /// The regular expression for validating credit card numbers in the app.
   static RegExp creditCardNumberRegex =
-      RegExp(r'^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$');
+      RegExp(r'^(?:4[0-9]{12}(?:[0-9]{3})?|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$');
 
   /// The regular expression for validating credit card CVV in the app.
   static RegExp creditCardCVVRegex = RegExp(r'^[0-9]{3}$');
